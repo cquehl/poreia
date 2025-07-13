5cfa6a89b674bb3074d2fd6e55f6439ab789bad0
CJQ <cjq@CJQs-MacBook-Pro.local>
2025-07-13 18:21:45 -0400

    hot reloads

--- src/ui/Menu.tsx
+++ src/ui/Menu.tsx
@@ -11,7 +11,7 @@ export const Menu: React.FC = () => {
         <div className="menu-container">
             <h1 className="title-green">Wilderness Survival</h1>
             <button className="clickable" onClick={handleStart}>Start Game</button>
-            <button className="clickable" disabled>Quit</button>
+            <button className="clickable" disabled>Credits</button>
         </div>
     );
 };
